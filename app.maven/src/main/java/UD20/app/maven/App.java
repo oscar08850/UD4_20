6583cd24ea58ccbdd6ca4031c13646bd59c0c720
Oscar Vilamitjana <oscar08850>
2022-10-26 14:02:18 +0200

    Comit de creacion de proyecto Maven

---
+++ app.maven/src/main/java/UD20/app/maven/App.java
@@ -0,0 +1,13 @@
+package UD20.app.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
